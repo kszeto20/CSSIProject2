54e9daa4eee0fb407f9110583d07f7f86418dfd7
Kirsten Szeto <73085697+kszeto20@users.noreply.github.com>
2022-07-22 15:47:22 +0000

    playlist adding fixed; no duplicates allowed

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ console.log("script running");
 const cart = document.querySelector("#cart");
 cartItems = [];
 
-playlist = [];
 
 let button1 = document.querySelector("#three-body-problem");
 let buttons = document.querySelectorAll(".add-button");
@@ -10,12 +9,17 @@ let buttons = document.querySelectorAll(".add-button");
 //console.log(buttons);
 //button1.addEventListener('click', (e) => {
 buttons.forEach(button => button.addEventListener('click', (e) => {
-   console.log(button.id);
+  console.log(button.id);
+  if (cartItems.indexOf(button.id) == -1) {
+    //let bookName = "Three Body Problem";
+    cartItems.push(button.id);
+    cartItems.sort();
+    // This next line is gross. Make it nicer.
+    cart.innerHTML = "<UL><LI><b>" + cartItems.join("</b></LI><LI><b>") + "</b></LI></UL>";
+    console.log("Added " + button.id);
+    
+  }
 
-  //let bookName = "Three Body Problem";
-  cartItems.push(button.id);
-  cartItems.sort();
-  // This next line is gross. Make it nicer.
-  cart.innerHTML = "<UL><LI><b>" + cartItems.join("</b></LI><LI><b>") + "</b></LI></UL>";
-  console.log("Added " + button.id);
+    
+   
 }));
