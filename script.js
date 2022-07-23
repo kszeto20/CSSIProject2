84a771749093dca4a4c73c3aef4e7b655041b741
Kirsten Szeto <73085697+kszeto20@users.noreply.github.com>
2022-07-23 04:55:53 +0000

    phoebe and lana card work with full playlist addition functionality

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let buttons = document.querySelectorAll(".add-button");
 //console.log(buttons);
 //button1.addEventListener('click', (e) => {
 buttons.forEach(button => button.addEventListener('click', (e) => {
-  console.log(button.id);
+  console.log(button.dataset.name);
   if (cartItems.indexOf(button.id) == -1) {
     //let bookName = "Three Body Problem";
     cartItems.push(button.id);
@@ -17,9 +17,7 @@ buttons.forEach(button => button.addEventListener('click', (e) => {
     // This next line is gross. Make it nicer.
     cart.innerHTML = "<UL><LI><b>" + cartItems.join("</b></LI><LI><b>") + "</b></LI></UL>";
     console.log("Added " + button.id);
-    
+
   }
 
-    
-   
 }));
