45f190430e59056439e167c29150f97fbb5530e8
Kirsten Szeto <73085697+kszeto20@users.noreply.github.com>
2022-07-25 13:19:27 +0000

    adding feature fixed

--- script.js
+++ script.js
@@ -10,14 +10,25 @@ let buttons = document.querySelectorAll(".add-button");
 //button1.addEventListener('click', (e) => {
 buttons.forEach(button => button.addEventListener('click', (e) => {
   console.log(button.dataset.name);
-  if (cartItems.indexOf(button.id) == -1) {
+  if (cartItems.indexOf(button.dataset.name) == -1) {
     //let bookName = "Three Body Problem";
-    cartItems.push(button.id);
+    cartItems.push(button.dataset.name);
     cartItems.sort();
     // This next line is gross. Make it nicer.
     cart.innerHTML = "<UL><LI><b>" + cartItems.join("</b></LI><LI><b>") + "</b></LI></UL>";
     console.log("Added " + button.id);
-
+  }
+  else {
+    if (confirm('Song is already in playlist.  Would you like to add it again?')) {
+      // Save it!
+      cartItems.push(button.id);
+      cartItems.sort();
+      cart.innerHTML = "<UL><LI><b>" + cartItems.join("</b></LI><LI><b>") + "</b></LI></UL>";
+      console.log("Added " + button.id);
+    } else {
+      // Do nothing!
+      console.log('Song was not added.');
+    }
   }
 
 }));
